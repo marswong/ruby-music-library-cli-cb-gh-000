ff8a9db6572089fe1f4f639c6197e55845b7af67
Mars Wong <marswong618@gmail.com>
2018-02-01 07:29:00 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,8 +5,8 @@ class Song
 
   def initialize(name, artist = nil, genre = nil)
     @name = name
-    @artist = artist
-    @genre = genre
+    artist = artist
+    genre = genre
   end
 
   def self.all
