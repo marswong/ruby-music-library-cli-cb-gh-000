3f480ea4f91e6561efbcc11faf3f6554f69a0a86
Mars Wong <marswong618@gmail.com>
2018-02-01 05:54:08 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,11 @@
 class Artist
-  attr_accessor :name, :songs
+  attr_accessor :name, :songs, :genres
   @@all = []
 
   def initialize(name)
     @name = name
     @songs = []
+    @genres = []
     self.save
   end
 
