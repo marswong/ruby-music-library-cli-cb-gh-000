090ea507446d02a23dc281a5e03081afae4362ec
Mars Wong <marswong618@gmail.com>
2018-02-01 07:35:47 +0000

    automatically backed up by learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -9,6 +9,6 @@ class MusicImporter
   end
 
   def import
-    @files.each { |filename| Song.new_from_filename(filename) }
+    @files.each { |filename| Song.create_from_filename(filename) }
   end
 end
