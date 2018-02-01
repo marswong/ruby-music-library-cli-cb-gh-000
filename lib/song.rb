f03d93a8678660ab79eb70686b349bdcc99b4d35
Mars Wong <marswong618@gmail.com>
2018-02-01 07:30:09 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,6 +9,16 @@ class Song
     @genre = genre
   end
 
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.songs << self unless genre.songs.include?(self)
+  end
+
   def self.all
     @@all
   end
@@ -44,15 +54,5 @@ class Song
 
   def save
     self.class.all << self
-  end
-
-  def artist=(artist)
-    @artist = artist
-    artist.add_song(self)
-  end
-
-  def genre=(genre)
-    @genre = genre
-    genre.songs << self unless genre.songs.include?(self)
   end
 end
