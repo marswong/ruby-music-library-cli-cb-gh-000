b2724f77bfb13ed0b0e7bfbe60076603d3f849be
Mars Wong <marswong618@gmail.com>
2018-02-01 06:22:14 +0000

    automatically backed up by learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -1,2 +1,14 @@
 class MusicImporter
+  attr_accessor :path, :files
+
+  def initialize(path)
+    if Dir.exist?(path)
+      @path = path
+      @files = Dir.entries(path).select { |filename| filename.end_with?(".mp3") }
+    end
+  end
+
+  def import
+    @files.each { |filename| Song.new_by_filename(filename) }
+  end
 end
