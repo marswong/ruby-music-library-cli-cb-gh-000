583cacf986eecfec8298923ce38a87ce2bd6e03a
Mars Wong <marswong618@gmail.com>
2018-02-01 07:49:59 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -30,7 +30,7 @@ class Artist
 
   def add_song(song)
     song.artist = self unless song.artist
-    songs << song unless @songs.include?(song)
+    songs << song unless songs.include?(song)
   end
 
   def genres
