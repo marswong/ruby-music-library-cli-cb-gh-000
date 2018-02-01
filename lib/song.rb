ed2c8c6bca842369c2e34390addec41ac02979c6
Mars Wong <marswong618@gmail.com>
2018-02-01 06:23:15 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,6 +31,16 @@ class Song
     self.find_by_name(name) || self.create(name)
   end
 
+  def self.new_by_filename(filename)
+    arr = filename.split(" - ")
+    song = self.new(arr[1])
+    artist = Artist.new(arr[0])
+    artist.songs << song
+    song.artist = artist
+    artist.save
+    song
+  end
+
   def save
     @@all << self
   end
