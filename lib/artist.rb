e5aace37670d6bc7c8d4685cd0ffbf850876db3a
Mars Wong <marswong618@gmail.com>
2018-02-01 07:49:38 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -30,10 +30,10 @@ class Artist
 
   def add_song(song)
     song.artist = self unless song.artist
-    @songs << song unless @songs.include?(song)
+    songs << song unless @songs.include?(song)
   end
 
   def genres
-    songs.map{ |song| song.genre }.uniq    
+    songs.map{ |song| song.genre }.uniq
   end
 end
