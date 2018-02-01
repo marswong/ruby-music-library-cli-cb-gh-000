b9b8a595f6918f07928d38700ed6fbbf3b93b4fb
Mars Wong <marswong618@gmail.com>
2018-02-01 06:07:04 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,6 +27,10 @@ class Song
     @@all.bsearch { |song| song.name == name }
   end
 
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create(name)
+  end
+
   def save
     @@all << self
   end
