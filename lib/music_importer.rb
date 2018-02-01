1e8e8f805772c5714c1a73508de593736ee8c082
Mars Wong <marswong618@gmail.com>
2018-02-01 07:54:49 +0000

    automatically backed up by learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -1,33 +1,14 @@
-# class MusicImporter
-#   attr_accessor :path, :files
-# 
-#   def initialize(path)
-#     if Dir.exist?(path)
-#       @path = path
-#       @files = Dir.entries(path).select { |filename| filename.end_with?(".mp3") }
-#     end
-#   end
-# 
-#   def import
-#     @files.each { |filename| Song.create_from_filename(filename) }
-#   end
-# end
-
-
 class MusicImporter
-  
-  attr_accessor :path
-  
+  attr_accessor :path, :files
+
   def initialize(path)
-    @path = path
+    if Dir.exist?(path)
+      @path = path
+      @files = Dir.entries(path).select { |filename| filename.end_with?(".mp3") }
+    end
   end
-  
-  def files
-    Dir.glob("#{path}/*.mp3").collect{ |x| x.gsub("#{path}/", "") }
+
+  def import
+    @files.each { |filename| Song.create_from_filename(filename) }
   end
-  
-  def import 
-    files.each{ |file| Song.create_from_filename(file) }
-  end
-  
 end
