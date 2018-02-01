6debba542ceff7e89e08f9fd465a49b992287700
Mars Wong <marswong618@gmail.com>
2018-02-01 05:59:50 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,10 +1,11 @@
 class Genre
-  attr_accessor :name, :songs
+  attr_accessor :name, :songs, :artists
   @@all = []
 
   def initialize(name)
     @name = name
     @songs = []
+    @artists = []
     self.save
   end
 
